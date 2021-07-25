eb8312b5c14a87558da13f173c337984765bf4b0
Jihyeok11 <woah68@gmail.com>
2021-07-25 13:25:21 +0900

    21.07.25

--- Python/BAEKJOON/tony9402/자료구조1/1966_프린터큐.py
+++ Python/BAEKJOON/tony9402/자료구조1/1966_프린터큐.py
@@ -1,17 +1,26 @@
 import sys
 sys.stdin = open("1966in.txt", 'r')
 
+def pri(m):
+    cnt = point = 0
+    lv = 9
+    while lv:
+        for i in range(n):
+            if li[(point + i) % n][1] == lv:
+                tp = (point + i) % n
+                cnt += 1
+                if li[tp][0] == m:
+                    return cnt
+        lv -= 1
+        try:
+            point = tp
+        except:
+            pass
+
 for _ in range(int(sys.stdin.readline())):
     n, m = map(int,sys.stdin.readline().split())
-    ba = list(map(int, sys.stdin.readline().split()))
-    val = ba[m]
-    answer = 0
-    level = 9
-    while True:
-        for i in range(len(ba)):
-            if ba[i] == level:
-                ba = ba[:i] + ba[i+1]
-                answer += 1
-
-        else:
-            level -= 1
+    t = list(map(int,input().split()))
+    li = []
+    for i in range(n):
+        li.append((i,t[i]))
+    print(pri(m))
