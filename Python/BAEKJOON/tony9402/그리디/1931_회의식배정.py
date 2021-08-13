801613b27510345e5a2d817743285369ef082520
Jihyeok11 <woah68@gmail.com>
2021-08-13 21:49:29 +0900

    21.08.13

---
+++ Python/BAEKJOON/tony9402/그리디/1931_회의식배정.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open("1931in.txt",'r')
+
+n = int(sys.stdin.readline())
+li = []
+for _ in range(n):
+    a,b = map(int,sys.stdin.readline().split())
+    li.append((a,b))
+li = sorted(li,key=lambda x:x[0])
+li = sorted(li,key=lambda x:x[1])
+ed = ans = 0
+for i in range(len(li)):
+    if li[i][0] >= ed:
+        ed = li[i][1]
+        ans += 1
+print(ans)
