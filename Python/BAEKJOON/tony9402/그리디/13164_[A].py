2ec91411199840be9fe80cd4b3785a68ca47f1b9
Jihyeok11 <woah68@gmail.com>
2021-08-24 13:54:46 +0900

    21.08.24

--- Python/BAEKJOON/tony9402/그리디/13164_[A].py
+++ Python/BAEKJOON/tony9402/그리디/13164_[A].py
@@ -1,19 +1,14 @@
-import sys
+import sys, heapq
 sys.stdin = open("13164in.txt", 'r')
 input = sys.stdin.readline
- 
+    
 n,k = map(int,input().split())
 arr = list(map(int,input().split()))
-result = 0
 temp = [] 
-for i in range(1,n):
-    temp.append(arr[i] - arr[i-1])
-temp.sort()
- 
-if k == n:
-    result = 0
-    print(result)
-else:
-    for i in range(n-k):
-        result += temp[i]
-    print(result)
+result = 0
+for i in range(n - 1):
+    heapq.heappush(temp, arr[i+1] - arr[i])
+    # temp.append(arr[i + 1] - arr[i])
+for i in range(n-k):
+    result += temp[i]
+print(result)
--- Python/BAEKJOON/tony9402/그리디/13164_행복유치원.py
+++ Python/BAEKJOON/tony9402/그리디/13164_행복유치원.py
@@ -1,11 +1,13 @@
-import sys
+import sys, heapq
 sys.stdin = open("13164in.txt", 'r')
 
 n, k = map(int, sys.stdin.readline().split())
 li = sorted(list(map(int ,sys.stdin.readline().split())), key= lambda x:x)
-print(li)
-team = []
-for i in li:
-    if len(team) < k:
-        team.append((i, i))
-print(team)
+gaps = []
+ans = 0
+for i in range(n - 1):
+    heapq.heappush(gaps, li[i+1] - li[i])
+for i in range(n - k):
+    ans += gaps[i]
+gaps.sort()
+print(ans)
