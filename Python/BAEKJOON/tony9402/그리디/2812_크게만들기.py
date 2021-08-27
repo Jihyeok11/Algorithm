376888dbd3e4bfa93bed69cc78a2a022eaefb543
Jihyeok11 <woah68@gmail.com>
2021-08-27 11:43:28 +0900

    21.08.27

--- Python/BAEKJOON/tony9402/그리디/2812_크게만들기.py
+++ Python/BAEKJOON/tony9402/그리디/2812_크게만들기.py
@@ -3,12 +3,10 @@ sys.stdin = open("2812in.txt", 'r')
 
 n, k = map(int, sys.stdin.readline().strip().split())
 li =  list(int(x) for x in sys.stdin.readline().strip())
-res = []
+K, res = k, []
 for i in range(n):
-    for j in res:
-        if li[i] > res[j]:
-            res.pop(j)
-            k -= 1
-    else:
-        res.append(li[i])
-print(res)
+    while res and K and res[-1] < li[i]:
+        res.pop()
+        K -= 1
+    res.append(li[i])
+print("".join(list(str(x) for x in res[:n-k])))
