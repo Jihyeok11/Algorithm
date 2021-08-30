17cdf6093fc15bc49be6a5c3655b347a4d4390e3
Jihyeok11 <woah68@gmail.com>
2021-08-31 04:01:17 +0900

    32

--- Python/BAEKJOON/tony9402/그리디/1092_배.py
+++ Python/BAEKJOON/tony9402/그리디/1092_배.py
@@ -6,17 +6,23 @@ container = sorted(list(map(int, sys.stdin.readline().strip().split())), key=lam
 m = int(sys.stdin.readline())
 boxes = sorted(list(map(int, sys.stdin.readline().strip().split())), key=lambda x:x, reverse=True)
 
+# 옮겨 졌는지 확인을 위한 boxes 크기의 리스트를 만든다 
+vi = list(True for _ in range(m))
+position = list(0 for _ in range(n))
+answer = 0
 if boxes[0] > container[0]:
     print(-1)
 else:
-    locations = []
-    cnt = 0
-    idx = 0
-    for c in range(n):
-        while boxes[idx] > container[c] and idx < m:
-            idx += 1
-        if idx < m:
-            locations.append(idx)
-    res = []
-    for i in
-    print(locations)
+    while any(vi):
+        idx = 0
+        for cont in range(len(container)):
+            for box in range(len(boxes)):
+                while position[box] < m:
+                    if container[cont] >= boxes[box] and vi[box]:
+                        position[box] += 1
+                        
+
+
+
+        answer += 1
+print(answer)
