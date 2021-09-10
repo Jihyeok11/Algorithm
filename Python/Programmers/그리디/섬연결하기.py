d31152187b708ff045ae758a37bb3a26a1637b7d
Jihyeok11 <woah68@gmail.com>
2021-09-10 22:52:39 +0900

    21.09.10

--- Python/Programmers/그리디/섬연결하기.py
+++ Python/Programmers/그리디/섬연결하기.py
@@ -4,29 +4,26 @@ sys.stdin = open("섬연결하기in.txt", 'r')
 
 
 def solution(n, costs):
+    answer = 0
     vi = list(True for _ in range(n))
     town = defaultdict(list)
     for i in costs:
         town[i[0]].append([i[1], i[2]])
         town[i[1]].append([i[0], i[2]])
-    s = 0
+    # 시작점의 위치를 0
+    vi[0] = False
     li = []
+    # 0에서 이동할 수 있는 곳을 li에 저장한다. 가장 짧은 길로 가면 되므로 길을 기준으로 heappush해준다.
     for i in town[0]:
-        heapq.heappush(li, i)
-    
-    answer = 0
+        heapq.heappush(li, [i[1], i[0]])
+    # 모든 길은 연결 되어 있으므로, 모든 길을 다 돌때까지 실행하면 된다.
+    while li:
+        length, dest = heapq.heappop(li)
+        if vi[dest]:
+            vi[dest] = False
+            answer += length
+            for i in town[dest]:
+                heapq.heappush(li, [i[1], i[0]])
     return answer
 
-
-n = int(sys.stdin.readline())
-li = sys.stdin.readline().strip()
-li = li.replace("],[", "/")
-li = li.replace(",", "")
-li = li.replace("[", "")
-li = li.replace("]", "")
-tp = li.split("/")
-li = []
-for i in range(n):
-    li.append(list(int(x) for x in tp[i]))
-
-print(solution(n, li))
+print(solution(4,[[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]))
