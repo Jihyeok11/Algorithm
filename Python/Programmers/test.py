d9187da3f83d2e74b5ad99d6cf434dedf1522c15
Jihyeok11 <woah68@gmail.com>
2021-09-12 16:57:29 +0900

    21.09.12

--- Python/Programmers/test.py
+++ Python/Programmers/test.py
@@ -1,14 +1,20 @@
-a = [7, 2, 3, 8, 4, 5 ]
-dp = [0] * len(a)
+def rotate_90(n, List):
+    cnt = 0
+    l = len(List)
+    li = list(list(0 for _ in range(l)) for _ in range(l))
+    while cnt < n:
+        for y in range(l):
+            for x in range(l):
+                li[x][l - 1 - y] = List[y][x]
+        cnt += 1
+        for y in range(l):
+            for x in range(l):
+                List[y][x] = li[y][x]
+    return li
 
-for i in range(1, len(a)):
-    for j in range(i - 1, -1, -1):
-        if a[i] > a[j]:
-            if dp[i] < dp[j] + 1:
-                dp[i] = dp[j] + 1
-print(dp)
-Max = 0
-for i in range(len(dp)):
-    if Max < dp[i]:
-        Max = dp[i]
-print(Max)
+
+a = [[1,2,3], [4,5,6], [7,8,9]]
+print(rotate_90(1,a))
+print(rotate_90(1,a))
+print(rotate_90(1,a))
+print(rotate_90(1,a))
