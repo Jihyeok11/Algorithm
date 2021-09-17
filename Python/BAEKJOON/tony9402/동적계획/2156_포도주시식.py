766fcae1f8a072fe1b7292b224cfc15eec5a5e73
Jihyeok11 <woah68@gmail.com>
2021-09-17 12:52:14 +0900

    21.09.17

--- Python/BAEKJOON/tony9402/동적계획/2156_포도주시식.py
+++ Python/BAEKJOON/tony9402/동적계획/2156_포도주시식.py
@@ -1,2 +1,14 @@
 import sys
 sys.stdin = open("2156in.txt", 'r')
+
+n = int(sys.stdin.readline())
+li = list(int(sys.stdin.readline()) for _ in range(n))
+result = [0, 0, 0] + li
+for i in range(n):
+    if i >= 2:
+        result[i + 3] = max(result[i] + li[i - 1] + li[i], result[i + 1] + li[i], result[i + 2])
+    elif i >= 1:
+        result[i + 3] = max(result[i] + li[i - 1] + li[i], result[i + 2])
+    else:
+        result[i + 3] = li[i]
+print(result[-1])
