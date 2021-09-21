a45b56956e637eab340e652afe4c112d0e80ce91
Jihyeok11 <woah68@gmail.com>
2021-09-21 17:39:54 +0900

    21.09.21

--- Python/Programmers/test.py
+++ Python/Programmers/test.py
@@ -1,3 +1,14 @@
-a = [3,4,5,6,7]
-print(a.index(4))
-print(range(0,4))
+a = [7, 2, 3, 8, 4, 5 ]
+dp = [0] * len(a)
+
+for i in range(1, len(a)):
+    for j in range(i - 1, -1, -1):
+        if a[i] > a[j]:
+            if dp[i] < dp[j] + 1:
+                dp[i] = dp[j] + 1
+print(dp)
+Max = 0
+for i in range(len(dp)):
+    if Max < dp[i]:
+        Max = dp[i]
+print(Max)
