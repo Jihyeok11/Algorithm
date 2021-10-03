f8848066b5d5f273004eb8380781a71be370db8d
Jihyeok11 <woah68@gmail.com>
2021-10-03 19:14:57 +0900

    21.10.03

---
+++ Python/BAEKJOON/tony9402/동적계획/9084_[A].py
@@ -0,0 +1,2 @@
+import sys
+sys.stdin = open("9084in.txt", 'r')
--- Python/BAEKJOON/tony9402/동적계획/9084_동전.py
+++ Python/BAEKJOON/tony9402/동적계획/9084_동전.py
@@ -1,9 +1,16 @@
 import sys
 sys.stdin = open("9084in.txt", 'r')
 
+
 for _ in range(int(sys.stdin.readline())):
     n = int(sys.stdin.readline())
-    a, b = map(int, sys.stdin.readline().strip().split())
-    money = int(sys.stdin.readline())
-    print(n, a, b, money)
-    
+    li = list(map(int, sys.stdin.readline().strip().split()))[::-1]
+    total = int(sys.stdin.readline())
+    dp = list(0 for _ in range(total + 1))
+    dp[0] = 1
+    for coin in li:
+        
+        for i in range(1, total+1):
+            if i - coin >= 0:
+                dp[i] += dp[i - coin]
+    print(dp[-1])
