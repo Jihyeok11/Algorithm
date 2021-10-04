a690fbcb3c306efd879156b68b314dab4454d66b
Jihyeok11 <woah68@gmail.com>
2021-10-04 21:23:28 +0900

    21.10.04

--- Python/BAEKJOON/tony9402/동적계획/10844_쉬운계단수.py
+++ Python/BAEKJOON/tony9402/동적계획/10844_쉬운계단수.py
@@ -1,22 +1,2 @@
 import sys
 sys.stdin = open("10844in.txt", 'r')
-
-def stairs(n, num):
-    global answer
-    if len(num) == n:
-        answer += 1
-        return
-    if int(num[-1]) == 0:
-        stairs(n, num + "1")
-    elif int(num[-1]) == 9:
-        stairs(n, num + "8")
-    else:
-        stairs(n, num + str(int(num[-1]) + 1))
-        stairs(n, num + str(int(num[-1]) - 1))
-            
-
-n = int(sys.stdin.readline())
-answer = 0
-for i in range(1, 10):
-    stairs(n, str(i))
-print(answer % 1000000000)
