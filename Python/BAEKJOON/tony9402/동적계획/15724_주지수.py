2abea2fd98bf2cb49d94d72065c44488767d7488
Jihyeok11 <woah68@gmail.com>
2021-10-18 23:24:40 +0900

    21.10.18

--- Python/BAEKJOON/tony9402/동적계획/15724_주지수.py
+++ Python/BAEKJOON/tony9402/동적계획/15724_주지수.py
@@ -1,12 +1,12 @@
 import sys
 sys.stdin = open("15724in.txt", 'r')
 
-n, m = map(int, sys.stdin.readline().split())
-li = list(list(map(int, sys.stdin.readline().split())) for _ in range(n))
+n, m = map(int, sys.stdin.readline().strip().split())
+li = list(list(map(int, sys.stdin.readline().strip().split())) for _ in range(n))
 ba = list(list(0 for _ in range(m+1)) for _ in range(n+1))
 for y in range(1, n+1):
     for x in range(1, m+1):
-        ba[y][x] = ba[y-1][x] + ba[y][x-1] + li[y-1][x-1] - ba[y-1][x-1]
+        ba[y][x] = li[y-1][x-1] + ba[y][x-1] + ba[y-1][x] - ba[y-1][x-1]
 for _ in range(int(sys.stdin.readline())):
-    y1, x1, y2, x2 = map(int, sys.stdin.readline().split())
+    y1, x1, y2, x2 = map(int,sys.stdin.readline().strip().split())
     print(ba[y2][x2] - ba[y2][x1-1] - ba[y1-1][x2] + ba[y1-1][x1-1])
