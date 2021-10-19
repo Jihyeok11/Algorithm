525beab33646d77276311678df04005cc60f9bd2
Jihyeok11 <woah68@gmail.com>
2021-10-20 01:34:17 +0900

    21.10.20

--- Python/BAEKJOON/tony9402/동적계획/1713_후보추천하기.py
+++ Python/BAEKJOON/tony9402/동적계획/1713_후보추천하기.py
@@ -5,14 +5,24 @@ sys.stdin = open("1713in.txt",'r')
 n = int(sys.stdin.readline())
 st = int(sys.stdin.readline())
 rec_st = defaultdict(int)
+check_li = defaultdict(int)
 li = list(map(int, sys.stdin.readline().strip().split()))
-lastpick = []
 ba = []
-for i in li:
-    lastpick.append(i)
-    if len(ba) < n:
-        if not i in ba:
-            ba.append(i)
-        rec_st[i] += 1
-    else:
-        rec_st[i] += 1
+for i in range(st):
+    rec_st[li[i]] += 1
+    check_li[li[i]] = i
+    if not li[i] in ba:
+        if len(ba) < n:
+            ba.append(li[i])
+        else:
+            tp = []
+            for j in ba:
+                tp.append([rec_st[j], check_li[j], j])
+            tp = sorted(tp, key=lambda x:(x[0], -x[1]))
+            # print(tp[-1]) [1, 0, 2]
+            rm_idx = tp[-1][2]
+            rec_st[rm_idx] = 0
+            ba.pop(ba.index(rm_idx))
+            ba.append(li[i])
+ba.sort()
+print(*ba)
