710c80ce2f6d0199478528466bd0f27f3a1440cd
Jihyeok11 <woah68@gmail.com>
2021-10-19 16:54:56 +0900

    21.10.19

--- Python/BAEKJOON/tony9402/동적계획/1713_후보추천하기.py
+++ Python/BAEKJOON/tony9402/동적계획/1713_후보추천하기.py
@@ -10,16 +10,15 @@ li = list(map(int, sys.stdin.readline().strip().split()))
 ba = []
 for i in range(st):
     rec_st[li[i]] += 1
-    check_li[li[i]] = i
     if not li[i] in ba:
+        check_li[li[i]] = i
         if len(ba) < n:
             ba.append(li[i])
         else:
             tp = []
             for j in ba:
                 tp.append([rec_st[j], check_li[j], j])
-            tp = sorted(tp, key=lambda x:(x[0], -x[1]))
-            # print(tp[-1]) [1, 0, 2]
+            tp = sorted(tp, key=lambda x:(-x[0], -x[1]))
             rm_idx = tp[-1][2]
             rec_st[rm_idx] = 0
             ba.pop(ba.index(rm_idx))
