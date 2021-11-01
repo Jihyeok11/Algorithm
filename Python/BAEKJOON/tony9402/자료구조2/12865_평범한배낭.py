b9c425a36e5cfeeb33725e4e1cea984bcce27574
Jihyeok11 <woah68@gmail.com>
2021-11-01 16:08:54 +0900

    21.11.01

--- Python/BAEKJOON/tony9402/자료구조2/12865_평범한배낭.py
+++ Python/BAEKJOON/tony9402/자료구조2/12865_평범한배낭.py
@@ -10,4 +10,5 @@ for i in range(1, n+1):
             ba[i][j] = max(ba[i-1][j], ba[i-1][j - li[i-1][0]] + li[i-1][1])
         else:
             ba[i][j] = ba[i-1][j]
+            
 print(ba[-1][-1])
