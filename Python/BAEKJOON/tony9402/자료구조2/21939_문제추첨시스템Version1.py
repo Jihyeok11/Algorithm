68ccfffafbcb605cbfbc8950b83227a848484a0c
Jihyeok11 <woah68@gmail.com>
2021-11-03 16:06:52 +0900

    21.11.03

--- Python/BAEKJOON/tony9402/자료구조2/21939_문제추첨시스템Version1.py
+++ Python/BAEKJOON/tony9402/자료구조2/21939_문제추첨시스템Version1.py
@@ -6,4 +6,11 @@ for _ in range(int(sys.stdin.readline().strip())):
     li.append(list(map(int, sys.stdin.readline().strip().split())))
 li = sorted(li, key = lambda x : (x[1], x[0]))
 for _ in range(int(sys.stdin.readline().strip())):
-    
+    s = sys.stdin.readline().strip()
+    if s == "a":
+        li
+        
+    elif s == "r":
+        pass
+    elif s == "s":
+        pass
