d36656e63511757729b650a69f8dadc84e9bd9db
Jihyeok11 <woah68@gmail.com>
2021-11-05 16:06:19 +0900

    21.11.05

---
+++ Python/BAEKJOON/tony9402/자료구조2/12865_평범한배낭.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open("12865in.txt", 'r')
+
+n, k = map(int, sys.stdin.readline().split())
+li = list(list(map(int, sys.stdin.readline().split())) for _ in range(n))
+ba = list(list(0 for _ in range(k+1)) for _ in range(n+1))
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        if j >= li[i-1][0]:
+            ba[i][j] = max(ba[i-1][j], ba[i-1][j - li[i-1][0]] + li[i-1][1])
+        else:
+            ba[i][j] = ba[i-1][j]
+print(ba[-1][-1])
