60b04368d26bfc567a34c66ea7ef7b47378e71a8
Jihyeok11 <woah68@gmail.com>
2021-11-14 00:05:46 +0900

    21.11.14

--- Python/BAEKJOON/tony9402/수학/22864_피로도.py
+++ Python/BAEKJOON/tony9402/수학/22864_피로도.py
@@ -1,19 +1,12 @@
 import sys
-from collections import deque, defaultdict
 sys.stdin = open("22864in.txt",'r')
 
 A, B, C, M = map(int, sys.stdin.readline().split())
-ba = deque([(0, 0)])
-answer = 0
-clocks = defaultdict(int)
-for _ in range(24):
-    for i in range(len(ba)):
-        a, b = ba.popleft()
-        if answer < b:
-            answer = b
-        if a + A <= M and clocks[a+A] < b+B:
-            clocks[a+A] = B
-            ba.append((a+A, b+B))
-        ba.append((max(a-C, 0), b))
-print(ba)
-print(answer)
+p = w = 0
+for cnt in range(24):
+    if p + A <= M:
+        w += B
+        p += A
+    else:
+        p = max(0, p-C)
+print(w)     
