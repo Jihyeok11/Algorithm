2c2a85ea913fa074a6d99dd41bdc22d00f9d4539
Jihyeok11 <woah68@gmail.com>
2021-11-18 22:50:14 +0900

    21.11.18

--- Python/Programmers/124_나라의_숫자.py
+++ Python/Programmers/124_나라의_숫자.py
@@ -1,11 +1,21 @@
 def solution(n):
-    n -= 1
+    num = [1, 2, 4]
     answer = ''
-
+    li = []
+    if n <= 3:
+        answer = num[n-1]
+    else:
+        tp = num[(n % 3) - 1]
+        while n:
+            li.append(n % 3)
+            n //= 3
+        for i in range(len(li)-1, -1, -1):
+            answer += str(num[li[i]-1])
+            
     return answer
 print(solution(1)) # 1
 print(solution(2)) # 2
 print(solution(3)) # 4
 # print(solution(4)) # 11
 print(solution(9)) # 24
-
+print(solution(83))
