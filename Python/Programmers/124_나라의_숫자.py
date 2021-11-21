6d8538bce365bca2018e6f67018fe880fffeee25
Jihyeok11 <woah68@gmail.com>
2021-11-21 23:16:17 +0900

    21.11.21

--- Python/Programmers/124_나라의_숫자.py
+++ Python/Programmers/124_나라의_숫자.py
@@ -1,20 +1,11 @@
-import math
 def solution(n):
-    num = [4, 1, 2]
-    answer = ''
-    li = []
-    cnt = 1
-    while n > 0:
-        tp = 3 ** cnt
-        li.append(num[math.ceil(((n % tp) / tp)*3)])
-        n -= tp
-        cnt += 1
-    print(li)
-            
-    return answer
+    if n<=3:
+        return '124'[n-1]
+    q, r = divmod(n-1, 3)
+    return solution(q) + '124'[r]
 print(solution(1)) # 1
 print(solution(2)) # 2
 print(solution(3)) # 4
 # print(solution(4)) # 11
 print(solution(9)) # 24
-print(solution(83))
+print(solution(83)) # 2242
--- Python/Programmers/test.py
+++ Python/Programmers/test.py
@@ -1,2 +1,5 @@
-import math
-print(math.ceil(3.000000))
+print('12345'[0])
+print('12345'[1])
+print('12345'[2])
+print('12345'[3])
+print('12345'[4])
