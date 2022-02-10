a8d5b0e5a8380f4d77a39969720b94633bc51f4f
Jihyeok11 <woah68@gmail.com>
2022-02-10 22:28:53 +0900

    22.02.10

---
+++ Python/BAEKJOON/tony9402/구현/1913_달팽이.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open("1913in.txt", 'r')
+
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+li = list(list(0 for _ in range(n)) for _ in range(n))
+y = x = n//2
+cnt = 1
+print(y, x)
+while cnt < n**2:
+    break
+    cnt += 1
+    
